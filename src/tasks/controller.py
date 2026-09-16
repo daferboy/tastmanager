47d53f009a351a573149a8dcecc954017bf38c6a
Nadeem <daferboy464@gmail.com>
2026-09-16 20:14:53 +0530

    deleted and update

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -1,6 +1,7 @@
 from src.tasks.dtos import TaskSchema
 from sqlalchemy.orm import Session
 from src.tasks.models import Task
+from fastapi import HTTPException
 
 def createtask(data:TaskSchema, db:Session):
     record = data.model_dump()
@@ -16,4 +17,41 @@ def createtask(data:TaskSchema, db:Session):
     return{
         "status":"OK",
         "record":task
+    }
+
+def get_tasks(db:Session):
+    tasks = db.query(Task).all()
+    #for task in tasks:
+    #   print(task.title, task.description)
+
+    return{
+        "status":"All Tasks",
+        "task":tasks
+    }
+
+def get_task(task_id:int, db:Session):
+    task = db.query(Task).get(task_id)
+    if not task:
+        return HTTPException(404, f"Task not found at id {task_id}")
+    return{
+        "Status":f"Task found at id {task_id}",
+        "task":task
+    }
+
+def update_task(task_id:int, db:Session, data:TaskSchema):
+    task = db.query(Task).get(task_id)
+    if not task:
+        return HTTPException(404, f"Task not found at id {task_id}")
+
+    task.title = data.title
+    task.description = data.description
+    task.isCompleted = data.isCompleted
+
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+
+    return{
+        "Status": "Task Updated",
+        "Updated Task": task
     }
--- src/tasks/router.py
+++ src/tasks/router.py
@@ -8,3 +8,15 @@ task_routes = APIRouter(prefix="/tasks")
 @task_routes.post("/createtask")
 def createtask(data:TaskSchema, db = Depends(get_db)):
     return controller.createtask(data, db)
+
+@task_routes.get("/")
+def get_tasks(db = Depends(get_db)):
+    return controller.get_tasks(db)
+
+@task_routes.get("/{task_id}")
+def get_task(task_id:int, db = Depends(get_db)):
+    return controller.get_task(task_id,db)
+
+@task_routes.put("/updatetask/{task_id}")
+def update_task(task_id:int, data:TaskSchema, db = Depends(get_db)):
+    return controller.update_task(task_id = task_id, data=data, db=db)
