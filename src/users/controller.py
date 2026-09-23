e89b9a5d98d5c85b465cd0a832ede4a405b4bcf7
daferboy <daferboy464@gmail.com>
2026-09-23 20:07:57 +0530

    jsw auth

--- src/users/controller.py
+++ src/users/controller.py
@@ -3,11 +3,17 @@ from src.users.dtos import UserSchema, LoginSchema
 from sqlalchemy.orm import Session
 from src.users.model import User
 from pwdlib import PasswordHash
+import jwt
+from datetime import datetime, timedelta
+from src.utils.settings import settings
 
 password_hash = PasswordHash.recommended()
 
 def get_password_hash(password):
     return password_hash.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
 
 def register(data: UserSchema, db:Session):
    #1. User validation
@@ -39,4 +45,18 @@ def login(credentials:LoginSchema, db:Session):
    user = db.query(User).filter(credentials.username == User.username).first()
    if not user:
       raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized username")
-   return "Done"
+
+
+   password_verify = verify_password(credentials.password, user.password)
+   # print(User.password)
+
+   if not password_verify:
+      raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized password")
+
+   exp_time = datetime.now() + timedelta(minutes=settings.EXP_TIME)
+
+   token = jwt.encode({"_user_id": user.id, "username": user.username, "exp": exp_time},settings.SECRET_KEY,settings.ALGORITHM)
+   
+   return {
+      "token": token
+   }
